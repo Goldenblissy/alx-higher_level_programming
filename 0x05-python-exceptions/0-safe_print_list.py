7d10b1aad62b92428d9daeeb5a9cd0f1a79890df
“Emmanuel <goldenblissy132@gmail.com>
2022-12-20 06:42:43 -0800

    Update task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+
+
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    try:
-        for i in range(0, x):
+    total = 0
+    for i in range(x):
+        try:
             print("{}".format(my_list[i]), end="")
-            count += 1
-        print()
-    except:
-        print()
-        return count
-    return count
+            total += 1
+        except IndexError:
+            break
+    print("")
+    return (total)
