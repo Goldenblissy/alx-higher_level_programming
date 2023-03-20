e98d8470189e72dd034c241ca0c8532e87a9dfb6
Emmanuel <goldenblissy132@gmail.com>
2023-03-20 22:43:44 +0100

    SOlution to task

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
-'''
-prints the State object with the name passed as argument
-'''
-
-
-from sys import argv
+"""Fetches state having given name"""
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
-
+from model_state import Base, State
 
 if __name__ == '__main__':
+    """
+    Access to the database and get the state having a
+    given name from the database.
+    """
+    db_username = sys.argv[1]
+    db_pass = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+    host = "localhost"
+    port = 3306
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
-    InstanceSession = sessionmaker(bind=engine)
-    session = InstanceSession()
-
-    state = session.query(State).filter(State.name == argv[4]).first()
-    print('Not found' if not state else state.id)
-    session.close()
+        f"mysql://{db_username}:{db_pass}@{host}:{port}/{db_name}")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(
+        State.name == state_name).order_by(State.id)
+    if (states.count() == 0):
+        print("Not found")
+    for state in states:
+        print(f"{state.id}")
--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-'''
-Deletes all State objects with a name containing the letter a
-'''
-
-
-from sys import argv
+"""adds a new state"""
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
-
+from model_state import Base, State
 
 if __name__ == '__main__':
+    """
+    Access to the database and adds a
+    new state to the database.
+    """
+    db_username = sys.argv[1]
+    db_pass = sys.argv[2]
+    db_name = sys.argv[3]
+    host = "localhost"
+    port = 3306
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
-    InstanceSession = sessionmaker(bind=engine)
-    session = InstanceSession()
-
-    states = session.query(State).filter(State.name.contains('a')).all()
-
+        f"mysql://{db_username}:{db_pass}@{host}:{port}/{db_name}")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name.like(f"%a%")).all()
     for state in states:
         session.delete(state)
-
     session.commit()
-    session.close()
